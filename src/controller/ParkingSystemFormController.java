c98c340bfc370133e6f047e73a0f2ef7761a8c5d
chamithgithub <dchamith4@gmail.com>
2023-10-12 17:01:04 +0530

    parking system ui add

---
+++ src/controller/ParkingSystemFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class ParkingSystemFormController {
+    public void selectVehicleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void parkVehicleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void onDiliveryOnAction(ActionEvent actionEvent) {
+    }
+
+    public void managementLogInOnAction(ActionEvent actionEvent) {
+    }
+}
